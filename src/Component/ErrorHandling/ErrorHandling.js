076978969205b4f3f7b618d60660bf226f7073c4
Shawn M (he/him) <shawnmcmahon17@gmail.com>
2021-08-08 11:52:22 -0600

    Create error component

---
+++ src/Component/ErrorHandling/ErrorHandling.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ErrorHandling = ({ errorMessage }) => {
+
+  return  (
+    <>
+      <h2>Error</h2>
+      <p> {errorMessage} </p>
+    </>
+  )
+
+
+
+}
+
+export default ErrorHandling; 
